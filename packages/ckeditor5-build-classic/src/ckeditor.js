7ea55dca168e5c430d199a0e8b32e76d23444b6e
Karan Hudia <karan.hudia@moia.io>
2021-01-11 14:30:14 +0100

    chore: add basic plugins by default

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -8,13 +8,20 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
@@ -29,6 +36,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -36,13 +44,20 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
+	Alignment,
 	Autoformat,
+	Autosave,
 	Bold,
 	Italic,
+	Underline,
+	Strikethrough,
+	Code,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
+	Font,
 	Heading,
+	Highlight,
 	Image,
 	ImageCaption,
 	ImageStyle,
@@ -56,7 +71,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	WordCount
 ];
 
 // Editor configuration.
@@ -67,9 +83,17 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'underline',
+			'strikethrough',
+			'code',
+			'alignment',
+			'fontColor',
+			'fontFamily',
+			'fontSize',
 			'link',
 			'bulletedList',
 			'numberedList',
+			'highlight',
 			'|',
 			'indent',
 			'outdent',
@@ -81,6 +105,9 @@ ClassicEditor.defaultConfig = {
 			'undo',
 			'redo'
 		]
+	},
+	wordCount: {
+		displayWords: true
 	},
 	image: {
 		toolbar: [
